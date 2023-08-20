daa161948f71f8181f86a244bd10cc5b582b110f
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-08-20 16:21:20 +0900

    style: :art: layout 적용 및 컨벤션 추가
    - RootLayout 생성 후 router 최상단에 적용
    - import React  생략

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RootLayout } from './layouts/RootLayout';
 
 export const App = () => {
 
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <div>
-          <h1>Hello World</h1>
-        </div>
-      ),
-    },
-    {
-      path: "about",
-      element: <div>About</div>,
+      element: <RootLayout />,
+      children: [
+        {
+          path: "/",
+          element: (
+            <div>
+              <h1>Hello World</h1>
+              <a href="./about">어바웃 이동</a>
+            </div>
+          ),
+        },
+        {
+          path: "about",
+          element: (
+            <div>
+              <h1>About</h1>
+              <a href="/"> 메인 이동</a>
+            </div>
+          ),
+        },
+      ]
     },
   ]);
   
