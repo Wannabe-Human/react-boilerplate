2abb8c0de3706ec330d733c9b939bbd9b1c98f51
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-08-21 16:03:07 +0900

    build: :wrench: tailwind : prod 빌드시 css 파일 크기 최소화

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  // prod 빌드시 사용하지 않는 css 를 제외하는 옵션 
+  // 참고: https://v1.tailwindcss.com/docs/controlling-file-size
+  purge: [ 
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
