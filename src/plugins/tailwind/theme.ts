84d6d9944105f1b451493ec9cc5951d482e2a7e9
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-08-21 18:01:47 +0900

    style: :art: tailwind : NotoSansKR fontfamily 추가

--- src/plugins/tailwind/theme.ts
+++ src/plugins/tailwind/theme.ts
@@ -3,7 +3,11 @@ import { Config } from 'tailwindcss';
 const theme : Config['theme'] = {
 
   //기본 테마에서 확장시킬때
-  extend : {}
+  extend : {
+    fontFamily: {
+      NotoSansKr: ['Noto Sans KR']
+    }
+  }
 };
 
 export default theme;
