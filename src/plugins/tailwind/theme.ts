1d9e09b76a2a1e3c1aa994a23f6e06e918f355cd
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-08-21 17:46:07 +0900

    style: :art: tailwind : theme 폴더 분리

---
+++ src/plugins/tailwind/theme.ts
@@ -0,0 +1,9 @@
+import { Config } from 'tailwindcss';
+
+const theme : Config['theme'] = {
+
+  //기본 테마에서 확장시킬때
+  extend : {}
+};
+
+export default theme;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 require('./src/plugins/tailwind/cssAsPlugin'); //css 파일을 tailwind plugins 로 변환하는 모듈, vscode 자동완성을 위해 적용하였다
 const CustomCSS = require('./src/plugins/tailwind/custom.css'); // 추가하려는 custom css, cssAsPlugin 모듈로 인해 플러그인으로 자동 변환된다
+const TailwindCustomTheme = require('./src/plugins/tailwind/theme'); // tailwind 테마
 
 /**
  * 
@@ -29,9 +30,7 @@ module.exports = {
   purge: [ 
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: TailwindCustomTheme,
   plugins: [
     CustomCSS
   ],
