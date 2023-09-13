adcab247dccdf5ff4e9697d2dbae5273bb4c0ddd
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-13 16:05:58 +0900

    feat: :sparkles: ImgBackground 컴포넌트
    - 애니메이션이 들어간 background는 이미지 로딩에 딜레이가 생겨, 순수하게 배경이미지만 첨부하는 컴포넌트 사용

---
+++ src/components/section/ImgBackground.tsx
@@ -0,0 +1,25 @@
+import { HTMLAttributes } from 'react';
+import { cn } from '@utils/cn';
+
+export interface ImgBackgroundProps extends HTMLAttributes<HTMLDivElement> {
+  src: string;
+}
+
+export const ImgBackground = ({
+  className: propsClassName,
+  src,
+  ...props
+}: ImgBackgroundProps) => {
+  return (
+    <section
+      className={cn('relative flex overflow-hidden', propsClassName)}
+      {...props}
+    >
+      <img
+        className='absolute inset-0 z-0 h-full w-full object-cover'
+        src={src}
+      />
+      <div className='z-10 flex h-full w-full'>{props.children}</div>
+    </section>
+  );
+};
