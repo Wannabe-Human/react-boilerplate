92016495b1a8c1754498cc62d4305978638743ac
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-13 15:52:53 +0900

    refactor: :triangular_flag_on_post: RootLayout 에 PartalSection - navigation_area, extra_area  정의

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,21 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loading } from '../components/loader/Loading';
+import { PortalSection } from '@components/portal/Portal';
 
 export const RootLayout = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Outlet />
-    </Suspense>
+    <>
+      {/* 화면 고정 영역 */}
+      <div className='pointer-events-none fixed inset-0 z-[100] flex h-full w-full flex-col'>
+        <div className='relative flex h-full w-full flex-col'>
+          <PortalSection portal_id='navigation_area' />
+          <PortalSection portal_id='extra_area' />
+        </div>
+      </div>
+      <Suspense fallback={<Loading />}>
+        <Outlet />
+      </Suspense>
+    </>
   );
 };
