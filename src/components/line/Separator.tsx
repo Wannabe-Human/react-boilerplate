d58212105940519a6d2e9a5725fbaccb45c23282
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-14 09:42:38 +0900

    feat: :sparkles: Separator 컴포넌트

---
+++ src/components/line/Separator.tsx
@@ -0,0 +1,29 @@
+import { forwardRef, ElementRef, ComponentPropsWithoutRef } from 'react';
+import { Root } from '@radix-ui/react-separator';
+
+import { cn } from '@utils/cn';
+
+const Separator = forwardRef<
+  ElementRef<typeof Root>,
+  ComponentPropsWithoutRef<typeof Root>
+>(
+  (
+    { className, orientation = 'horizontal', decorative = true, ...props },
+    ref,
+  ) => (
+    <Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        'flex shrink-0 bg-border',
+        orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+        className,
+      )}
+      {...props}
+    />
+  ),
+);
+Separator.displayName = Root.displayName;
+
+export { Separator };
