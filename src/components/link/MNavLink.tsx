049762a5b802ca2247d5fd9e8989b253f9f8b8b2
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-15 13:46:28 +0900

    feat: :sparkles: MNavLink 컴포넌트
    - 기존의 NavLink가 가진 pathname 만 확인할 수 있다는 제약을 극복하기 위해 만듦

---
+++ src/components/link/MNavLink.tsx
@@ -0,0 +1,60 @@
+import { useMemo } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import {
+  Link,
+  LinkProps,
+  useLocation,
+  useResolvedPath,
+} from 'react-router-dom';
+
+import { cn } from '@utils/cn';
+
+const MNavLinkVariants = cva('', {
+  variants: {},
+  defaultVariants: {},
+});
+
+export interface MNavLinkProps
+  extends LinkProps,
+    VariantProps<typeof MNavLinkVariants> {
+  isActiveClassName: (isActive: boolean) => string;
+  end?: boolean; //포함 관계 제외
+  hash?: boolean; //hash 비교 활성화
+  rootInclude?: boolean;
+}
+
+const MNavLink = ({
+  className,
+  isActiveClassName,
+  to,
+  end = false,
+  hash = false,
+  rootInclude = false,
+  ...props
+}: MNavLinkProps) => {
+  const loaction = useLocation();
+  const resolvedPath = useResolvedPath(to);
+
+  const isActive = useMemo(() => {
+    //포함 관계 확인
+    if (loaction.pathname.startsWith(resolvedPath.pathname)) {
+      if (resolvedPath.pathname == '/' && !rootInclude) return false;
+      if (end && loaction.pathname != resolvedPath.pathname) return false;
+      if (hash && loaction.hash != resolvedPath.hash) return false;
+      return true;
+    } else {
+      return false;
+    }
+  }, [resolvedPath, loaction, end, hash, rootInclude]);
+
+  return (
+    <Link
+      to={to}
+      className={cn(className, isActiveClassName(isActive))}
+      {...props}
+    />
+  );
+};
+MNavLink.displayName = 'MNavLink';
+
+export { MNavLink, MNavLinkVariants };
