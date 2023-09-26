188a1ea87cc2158fc83857a550ed78606eb9a37d
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-26 17:58:51 +0900

    feat: :sparkles: HexBeeHiveView UI 컴포넌트
    - 육각형의 벌집 모양의 UI를 만드는 컴포넌트

---
+++ src/components/view/HexBeehiveView.tsx
@@ -0,0 +1,71 @@
+import { HTMLAttributes } from 'react';
+import { cn, CSSVariables } from '@utils/tailwind';
+
+export interface HexBeehiveViewProps extends HTMLAttributes<HTMLDivElement> {
+  itemList: {
+    imgSrc?: string;
+  }[];
+  hexHeight?: number; //0~100
+  hexAngle?: number; //0~100
+  gap?: number;
+  lineLimit?: number; //1줄에 최대로 들어갈 수 있는 길이
+}
+
+export const HexBeehiveView = ({
+  className,
+  gap = 2,
+  hexHeight = 100,
+  hexAngle = 100,
+  itemList,
+  lineLimit = 5,
+  ...props
+}: HexBeehiveViewProps) => {
+  return (
+    <section
+      style={CSSVariables({
+        grid: {
+          'repeat-cols': `${lineLimit}`,
+          gap: `${gap * 2}px`,
+          'even-rows': {
+            offset: `${gap}px`,
+          },
+        },
+        hex: {
+          height: `${0.5 + hexHeight / 500}`,
+          angle: `${1.3 + hexAngle / 450}`,
+        },
+      })}
+      className={cn('grid-cols-repeat-custom grid gap-[--grid-gap]', className)}
+      {...props}
+    >
+      {itemList.map((item, i) => {
+        const isEven = Math.floor(i / lineLimit) % 2 == 1; // 짝수번째 줄의 item 일 경우
+        return (
+          <div
+            key={`item-hex-${i}`}
+            className={cn(
+              'invisible relative w-full after:block after:pb-[calc(calc(100%*var(--hex-height))+calc(10%*calc(var(--hex-angle)-1)))] after:content-[""]',
+            )}
+          >
+            <div
+              className={cn(
+                'rotate-3d-hex-rhombus invisible absolute top-0 w-full overflow-hidden pb-[100%]',
+                lineLimit % 2 == 1
+                  ? isEven
+                    ? 'left-[calc(50%+var(--grid-even-rows-offset))]'
+                    : ''
+                  : isEven
+                  ? 'left-[calc(25%+var(--grid-even-rows-offset))]'
+                  : 'left-[calc(-25%)]',
+              )}
+            >
+              <div className='rotate-3d-hex-rhombus-reverse visible absolute z-40 flex h-full w-full items-center justify-center overflow-hidden'>
+                <img className='h-full w-full object-cover' src={item.imgSrc} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
