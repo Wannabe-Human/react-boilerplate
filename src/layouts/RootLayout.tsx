0f171a4670301e36cd320e1354fb0a538b68dd38
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-09-27 11:09:10 +0900

    refactor: :triangular_flag_on_post: RootLayout 에useRootRouterScrollToHash, useRootAxiosInterceptor, ScrollRestoration 적용

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,23 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Loading } from '../components/loader/Loading';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Loading } from '@components/loader/Loading';
 import { PortalSection } from '@components/portal/Portal';
+import { useMediaQuery } from '@hooks/useMediaQuery';
+import { useRootRouterScrollToHash } from '@hooks/useRootRouterScrollToHash';
+import { useRootAxiosInterceptor } from '@hooks/useRootAxiosInterceptor';
+import { GLOBAL } from '@utils/tailwind';
 
 export const RootLayout = () => {
+  const { isMedia } = useMediaQuery();
+
+  useRootRouterScrollToHash({
+    offset: isMedia('lg', 'more')
+      ? Number(GLOBAL('header.height.pc').value)
+      : Number(GLOBAL('header.height.mobile').value),
+  });
+
+  useRootAxiosInterceptor();
+
   return (
     <>
       {/* 화면 고정 영역 */}
@@ -16,6 +30,11 @@ export const RootLayout = () => {
       <Suspense fallback={<Loading />}>
         <Outlet />
       </Suspense>
+      <ScrollRestoration
+        getKey={(loaction) => {
+          return loaction.key;
+        }}
+      />
     </>
   );
 };
