cd7db4b0a1db4f6732f21421195ddc22dc218f49
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-10-25 18:38:08 +0900

    refactor: :sparkles: GlobalAlert 사용을 위해 useAlert hook 재정의

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -1,24 +1,67 @@
 import { useBoolean } from 'usehooks-ts';
 
+import { useRecoil } from '@hooks/useRecoil';
+
+import { GlobalAlertDialogState } from '@components/alert/GlobalAlertDialogProvider';
+
 export const useAlert = () => {
   const { value, setTrue, setFalse } = useBoolean(false);
-  //로그아웃
-  const confirmClick = (click: () => void, questions: string) => {
-    if (value == true) return; //더블 클릭 방지용
+  const { useRecoilState, useAtom } = useRecoil();
+  const [state, setState] = useRecoilState(
+    useAtom<GlobalAlertDialogState>('ALERT_DIALOG_STATE'),
+  );
+
+  const confirmClick = (
+    questions: string | Pick<GlobalAlertDialogState, 'title' | 'description'>,
+    options?: Omit<
+      GlobalAlertDialogState,
+      'title' | 'description' | 'isOpen' | 'type'
+    >,
+  ) => {
+    if (value || state.isOpen) return; //더블 클릭 방지용
     setTrue(); //클릭 비활성화
 
-    if (window.confirm(questions)) {
-      click();
+    if (typeof questions == 'string') {
+      questions = {
+        title: '알림',
+        description: questions,
+      };
     }
+
+    setState({
+      isOpen: true,
+      type: 'confirm',
+      ...questions,
+      ...options,
+    });
 
     setTimeout(setFalse, 500); // 0.5초뒤 클릭 활성화
   };
 
-  const alertClick = (click: () => void, questions: string) => {
+  const alertClick = (
+    questions: string | Pick<GlobalAlertDialogState, 'title' | 'description'>,
+    options?: Omit<
+      GlobalAlertDialogState,
+      'title' | 'description' | 'isOpen' | 'type'
+    >,
+  ) => {
     if (value == true) return; //더블 클릭 방지용
     setTrue(); //클릭 비활성화
-    alert(questions);
-    click();
+
+    if (typeof questions == 'string') {
+      questions = {
+        title: '알림',
+        description: questions,
+      };
+    }
+
+    setState({
+      isOpen: true,
+      type: 'alert',
+      ...questions,
+      ...options,
+    });
+
     setTimeout(setFalse, 500); // 0.5초뒤 클릭 활성화
   };
 
