ce56b46a314c01670d2c70942a55988bf4284ec8
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-10-30 16:41:11 +0900

    feat: :sparkles: select parts 5종 외 기타 추가

---
+++ src/components/select/parts/index.tsx
@@ -0,0 +1,13 @@
+import * as SelectPrimitive from '@radix-ui/react-select';
+
+export * from '@components/select/parts/SelectContent';
+export * from '@components/select/parts/SelectItem';
+export * from '@components/select/parts/SelectLabel';
+export * from '@components/select/parts/SelectSeparator';
+export * from '@components/select/parts/SelectTrigger';
+
+export const Select = SelectPrimitive.Root;
+
+export const SelectGroup = SelectPrimitive.Group;
+
+export const SelectValue = SelectPrimitive.Value;
