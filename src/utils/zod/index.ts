01990a81cd692f66322d66dbe4b0f2f4629623f8
hyeonjun-commit <acabar0105navercom@gmail.com>
2023-10-30 15:06:47 +0900

    refactor: :triangular_flag_on_post: apiBoolean 변경

--- src/utils/zod/index.ts
+++ src/utils/zod/index.ts
@@ -1,7 +1,14 @@
 import * as z from 'zod';
 
-export const apiBoolean = z.preprocess(
-  //preprocess 가 제대로 동작하지 않아, checkbox 내부에서 default 값을 변경시킨다
-  (arg) => arg,
-  z.boolean().transform((val) => (val ? 1 : 0)), // 결과 형식을 1 or 0으로 변환),
-);
+export const apiBoolean = (defaultValue: number | boolean) =>
+  z
+    .boolean()
+    .transform((val) => (val ? 1 : 0))
+    .default(
+      typeof defaultValue == 'boolean' ? defaultValue : defaultValue == 1,
+    ); // 결과 형식을 1 or 0으로 변환),
+// z.preprocess(
+//   //preprocess 가 제대로 동작하지 않아, checkbox 내부에서 default 값을 변경시킨다
+//   (arg) => (typeof arg == 'boolean' ? arg : arg === 1),
+//   z.boolean().transform((val) => (val ? 1 : 0)), // 결과 형식을 1 or 0으로 변환),
+// );
